2cdbeab9d85759afd9ea6cc4029c94dae9dd9cc4
Max <maxim.macari.dam@gmail.com>
2020-12-14 12:22:19 +0100

    Initial Commit

---
+++ CustomCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomCamera
+//
+//  Created by Maxim Macari on 14/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomCamera/CustomCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCameraApp.swift
+//  CustomCamera
+//
+//  Created by Maxim Macari on 14/12/20.
+//
+
+import SwiftUI
+
+@main
+struct CustomCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
